719241b21afbf48a68a81d1ded76cac34136a531
Eduardo009cs <eduardo009cs@gmail.com>
2022-11-09 09:22:02 -0600

    Last update: 09-11-2022

--- Practica2/updateRRD.py
+++ Practica2/updateRRD.py
@@ -6,19 +6,19 @@ total_output_traffic = 0
 
 while 1:
     paqutesUnicast = int(
-        consultaSNMP('gustavoRomero','localhost',
-                     '1.3.6.1.2.1.2.2.1.11.10'))
+        consultaSNMP('eduardoCuevas','172.20.10.7',
+                     '1.3.6.1.2.1.2.2.1.11.3'))
     paquetesIPv4 = int(
-        consultaSNMP('gustavoRomero','localhost',
+        consultaSNMP('eduardoCuevas','172.20.10.7',
                      '1.3.6.1.2.1.4.3.0'))
     mensajesICMPe = int(
-        consultaSNMP('gustavoRomero','localhost',
+        consultaSNMP('eduardoCuevas','172.20.10.7',
                      '1.3.6.1.2.1.5.8.0'))
     segmentosRecibidos = int(
-        consultaSNMP('gustavoRomero','localhost',
+        consultaSNMP('eduardoCuevas','172.20.10.7',
                      '1.3.6.1.2.1.6.10.0'))
     datagramasEntregados = int(
-        consultaSNMP('gustavoRomero','localhost',
+        consultaSNMP('eduardoCuevas','172.20.10.7',
                      '1.3.6.1.2.1.4.3.0'))
     valor = "N:" + str(paqutesUnicast) + ':' + str(paquetesIPv4) + ':' + str(mensajesICMPe) + ':' + str(segmentosRecibidos) + ':' + str(datagramasEntregados)
     print (valor)
